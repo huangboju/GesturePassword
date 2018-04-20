6c855924223a3ca06e8dc5a6c3d500173293b553
huangboju <bhuang@xiaohongshu.com>
2018-04-20 15:15:18 +0800

    修改命名

--- Classes/LockStorage.swift
+++ Classes/LockStorage.swift
@@ -6,9 +6,9 @@ import Security
 
 protocol Storagable {
 
-    func setStr(_ value: String, key: String)
-    func str(forKey name: String) -> String?
-    func removeValue(forKey name: String)
+    func set(_ value: String, forKey key: String)
+    func str(forKey key: String) -> String?
+    func removeValue(forKey key: String)
 }
 
 struct LockKeychain: Storagable {
@@ -18,18 +18,18 @@ struct LockKeychain: Storagable {
     init() {
         keychain = Keychain()
     }
-
-    func setStr(_ value: String, key: String) {
+    
+    func set(_ value: String, forKey key: String) {
         keychain[key] = value
     }
 
-    func str(forKey name: String) -> String? {
-        return keychain[name]
+    func str(forKey key: String) -> String? {
+        return keychain[key]
     }
 
-    func removeValue(forKey name: String) {
+    func removeValue(forKey key: String) {
         do {
-            try keychain.remove(name)
+            try keychain.remove(key)
         } catch let error {
             print(error, "delete failure")
         }
@@ -44,60 +44,17 @@ struct LockUserDefaults: Storagable {
         defaults = UserDefaults.standard
     }
 
-    func setStr(_ value: String, key: String) {
+    func set(_ value: String, forKey key: String) {
         defaults.set(value, forKey: key)
         // 如果程序意外退出数据不会被系统写入到该文件，所以，要使用synchronize()命令直接同步到文件里
         defaults.synchronize()
     }
 
-    func str(forKey name: String) -> String? {
-        return defaults.string(forKey: name)
+    func str(forKey key: String) -> String? {
+        return defaults.string(forKey: key)
     }
 
-    func removeValue(forKey name: String) {
-        defaults.removeObject(forKey: name)
+    func removeValue(forKey key: String) {
+        defaults.removeObject(forKey: key)
     }
-
-    //    func setInt(_ value: Int, key: String) {
-    //        let defaults = UserDefaults.standard
-    //        defaults.set(value, forKey: key)
-    //        defaults.synchronize()
-    //    }
-    //
-    //    func int(forKey name: String) -> Int {
-    //        return UserDefaults.standard.integer(forKey: name)
-    //    }
-    //
-    //    func setFloat(_ value: Float, key: String) {
-    //        let defaults = UserDefaults.standard
-    //        defaults.set(value, forKey: key)
-    //        defaults.synchronize()
-    //    }
-    //
-    //    func float(forKey name: String) -> Float {
-    //        return UserDefaults.standard.float(forKey: name)
-    //    }
-    //
-    //    func setBool(_ value: Bool, key: String) {
-    //        let defaults = UserDefaults.standard
-    //        defaults.set(value, forKey: key)
-    //        defaults.synchronize()
-    //    }
-    //
-    //    func bool(forKey name: String) -> Bool {
-    //        return UserDefaults.standard.bool(forKey: name)
-    //    }
-
-    // MARK: - 归档
-    //    func archiveRoot(_ object: AnyObject, toFile path: String) -> Bool {
-    //        return NSKeyedArchiver.archiveRootObject(object, toFile: path)
-    //    }
-    //
-    //    func removeRootObjectWithFile(_ path: String) -> Bool {
-    //        return archiveRoot("" as AnyObject, toFile: path)
-    //    }
-    //
-    //    func unarchiveObjectWithFile(_ path: String) -> AnyObject? {
-    //        return NSKeyedUnarchiver.unarchiveObject(withFile: path) as AnyObject?
-    //    }
 }
