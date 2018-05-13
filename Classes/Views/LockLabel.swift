19d91ba4913e1a82c45e09262c3bd4221fb00104
huangboju <bhuang@xiaohongshu.com>
2018-05-13 13:48:15 +0800

    剥离LockManager.options

--- Classes/Views/LockLabel.swift
+++ Classes/Views/LockLabel.swift
@@ -2,28 +2,29 @@
 //  Copyright © 2016年 xiAo_Ju. All rights reserved.
 //
 
-class LockDescLabel: UILabel {
+open class LockDescLabel: UILabel {
 
-    private let options = LockManager.options
+    open var normalTextColor = UIColor(r: 192, g: 192, b: 192)
+    
+    open var warningTitleColor = UIColor(r: 254, g: 82, b: 92)
 
     override init(frame: CGRect) {
         super.init(frame: frame)
         textAlignment = .center
-        backgroundColor = options.backgroundColor
     }
 
     func showNormal(with message: String?) {
         text = message
-        textColor = options.normalTitleColor
+        textColor = normalTextColor
     }
 
     func showWarn(with message: String?) {
         text = message
-        textColor = options.warningTitleColor
+        textColor = warningTitleColor
         layer.shake()
     }
 
-    required init?(coder _: NSCoder) {
+    required public init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
