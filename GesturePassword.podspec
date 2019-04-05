39fbe33d5d695293451993c409ac3790a8b20d1d
huangboju <bhuang@xiaohongshu.com>
2019-04-05 11:41:11 +0800

    swift5

--- GesturePassword.podspec
+++ GesturePassword.podspec
@@ -2,7 +2,7 @@
 Pod::Spec.new do |s|
     
     s.name         = "GesturePassword"
-    s.version      = "2.0.3"
+    s.version      = "2.1.0"
     s.summary      = "GesturePassword is a simple gesture password written in Swift"
     s.homepage     = "https://github.com/huangboju/GesturePassword"
     s.license      = "MIT"
--- fabfile.py
+++ fabfile.py
@@ -32,6 +32,45 @@ def hello():
 def get_function_name():
     return sys._getframe(1).f_code.co_name  # _getframe()则是自己的名字
 
+
+@task
+def show_latest_tag():
+    tag = local('git describe --tags `git rev-list --tags --max-count=1`', capture=True)
+    puts(green('\n历史 tag 版本:   ' + tag + '\n'))
+   
+
+def find_podspec_file():
+    current_file = os.getcwd().split('/')[-1]
+    return current_file + '.podspec'
+
+
+@task
+def pod_version():
+    podspec_file = find_podspec_file()
+    f = open('./' + podspec_file, 'r')
+    result = ''
+    for line in f.readlines():
+        if 's.version' in line:
+            result = line
+            break
+    version = result.split('=')[1].replace("'", "").replace(" ", "").replace("\n", "")
+    f.close()
+    return version
+
+
+
+@task
+def pod_publish():
+    show_latest_tag()
+    version = pod_version()
+    tag = version
+    puts(green('\nspec tag 版本:   ' + version + '\n'))
+    message = raw_input(green("\n请输入 tag 信息:"))
+    local('git tag -a {} -m \'{}\''.format(tag, message))
+    local('git push origin {}'.format(tag))
+    pod_push()
+
 @task
 def pod_push():
     local('pod trunk push GesturePassword.podspec')
+
